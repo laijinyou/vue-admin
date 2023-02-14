c5367b76308d794032c085478738ad95dedb116a
laijinyou <1183820@qq.com>
2023-02-14 17:21:56 +0900

    Home view coding

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,31 @@
 <script setup lang="ts">
-//
+import { onMounted } from "vue";
+
+let currentOverview = [
+  {
+    name: "New products this week",
+    amount: 2340,
+    percent: 14.6,
+  },
+  {
+    name: "Visitors this week",
+    amount: 5355,
+    percent: 33.6,
+  },
+  {
+    name: "User signups this week",
+    amount: 2340,
+    percent: -14.6,
+  },
+];
+
+function formatNumber(num:int) {
+  return num.toLocaleString();
+}
+
+onMounted(() => {
+  //
+});
 </script>
 
 <template>
@@ -643,22 +669,31 @@
             <div
               class="mt-4 w-full grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4"
             >
-              <div class="bg-white shadow rounded-lg p-4 sm:p-6 xl:p-8">
+              <div
+                class="bg-white shadow rounded-lg p-4 sm:p-6 xl:p-8"
+                v-for="(item, index) in currentOverview"
+                :key="index"
+              >
                 <div class="flex items-center">
                   <div class="flex-shrink-0">
                     <span
                       class="text-2xl sm:text-3xl leading-none font-bold text-gray-900"
-                      >2,340</span
                     >
+                      {{ item.amount.toLocaleString() }}
+                    </span>
                     <h3 class="text-base font-normal text-gray-500">
-                      New products this week
+                      {{ item.name }}
                     </h3>
                   </div>
                   <div
-                    class="ml-5 w-0 flex items-center justify-end flex-1 text-green-500 text-base font-bold"
+                    class="ml-5 w-0 flex items-center justify-end flex-1 text-base font-bold"
+                    :class="
+                      item.percent > 0 ? 'text-green-500' : 'text-red-500'
+                    "
                   >
-                    14.6%
+                    {{ item.percent }}%
                     <svg
+                      v-if="item.percent > 0"
                       class="w-5 h-5"
                       fill="currentColor"
                       viewBox="0 0 20 20"
@@ -670,55 +705,8 @@
                         clip-rule="evenodd"
                       ></path>
                     </svg>
-                  </div>
-                </div>
-              </div>
-              <div class="bg-white shadow rounded-lg p-4 sm:p-6 xl:p-8">
-                <div class="flex items-center">
-                  <div class="flex-shrink-0">
-                    <span
-                      class="text-2xl sm:text-3xl leading-none font-bold text-gray-900"
-                      >5,355</span
-                    >
-                    <h3 class="text-base font-normal text-gray-500">
-                      Visitors this week
-                    </h3>
-                  </div>
-                  <div
-                    class="ml-5 w-0 flex items-center justify-end flex-1 text-green-500 text-base font-bold"
-                  >
-                    32.9%
                     <svg
-                      class="w-5 h-5"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        fill-rule="evenodd"
-                        d="M5.293 7.707a1 1 0 010-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 01-1.414 1.414L11 5.414V17a1 1 0 11-2 0V5.414L6.707 7.707a1 1 0 01-1.414 0z"
-                        clip-rule="evenodd"
-                      ></path>
-                    </svg>
-                  </div>
-                </div>
-              </div>
-              <div class="bg-white shadow rounded-lg p-4 sm:p-6 xl:p-8">
-                <div class="flex items-center">
-                  <div class="flex-shrink-0">
-                    <span
-                      class="text-2xl sm:text-3xl leading-none font-bold text-gray-900"
-                      >385</span
-                    >
-                    <h3 class="text-base font-normal text-gray-500">
-                      User signups this week
-                    </h3>
-                  </div>
-                  <div
-                    class="ml-5 w-0 flex items-center justify-end flex-1 text-red-500 text-base font-bold"
-                  >
-                    -2.7%
-                    <svg
+                      v-else
                       class="w-5 h-5"
                       fill="currentColor"
                       viewBox="0 0 20 20"
